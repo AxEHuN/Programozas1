5c9c113f68b5fbc7c1c71dd5048de0e8a1699b66
Alex <alexverebik123@gmail.com>
2022-02-22 16:04:34 +0100

    Ch14 Drill

---
+++ Drillek/chapter14/main.cpp
@@ -0,0 +1,29 @@
+#include "../std_lib_facilities.h"
+
+using namespace std;
+
+class B1
+	{
+	public:
+		virtual void vf()
+		{
+			cout << "B1::vf()" << endl;
+		}
+		void f()
+		{
+			cout << "B1::f()" << endl;
+		}
+	};
+
+	class 
+
+int main()
+{
+ 	B1 valami;
+ 	valami.vf();
+ 	valami.f();
+
+
+
+	return 0;
+}
