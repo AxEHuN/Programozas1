97cd9ce806a8403198494803e00c7857825eaeaa
Alex <alexverebik123@gmail.com>
2022-03-01 11:38:24 +0100

    Chapter 14 Drill

--- Drillek/chapter14/main.cpp
+++ Drillek/chapter14/main.cpp
@@ -7,21 +7,105 @@ class B1
 	public:
 		virtual void vf()
 		{
-			cout << "B1::vf()" << endl;
+			cout << "B1/vf()" << endl;
 		}
 		void f()
 		{
-			cout << "B1::f()" << endl;
+			cout << "B1/f()" << endl;
+		}
+
+		virtual void pvf() =0;
+	};
+
+	class D1 : public B1
+	{
+	public:
+		void vf() override
+		{
+			cout << "D1/vf()" << endl;
+		}
+		void f()
+		{
+			cout << "D1/f()" <<endl;
+		}
+
+	};
+
+	class D2 : public D1
+	{
+	public:
+		void pvf() override
+		{
+			cout << "D2/pvf()" << endl;
+		}
+	};
+	class B2
+	{
+	public:
+		virtual void pvf() = 0;
+	};
+
+	class D21 : B2
+	{
+		string s = "valami string";
+	public:
+
+		void pvf () override
+		{
+			cout << s << endl;
 		}
 	};
 
-	class 
+	/*class D22 : B2
+	{
+	int number = 69;
+	public:
+	void pvf() override
+		{
+			cout << number << end;
+		}
+	};
+	*/
+	/*void f(B2& j)
+	{
+		j.pvf();
+	}*/
+	
 
 int main()
 {
+ 	
  	B1 valami;
  	valami.vf();
  	valami.f();
+
+ 	cout << endl;
+
+ 	D1 masvalami;
+ 	masvalami.vf();
+ 	masvalami.f();
+
+ 	cout << endl;
+
+ 	/* B1& x {masvalami};
+ 	x.vf();
+ 	x.f();
+	*/ //Nem jó!
+
+	cout << endl;
+/*
+	D2 y;
+	y.vf();
+	y.f();
+	y.pvf();	
+	*/
+	cout << endl;
+
+	//D21 to;
+	//D22 tt;
+	//f(to);
+	//f(tt);
+
 
 
 
